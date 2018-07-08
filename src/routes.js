3a0006e9183e3a5426dd08f09fbcf99a45652a4d
Aryell Dias <aryelld@gmail.com>
2018-07-08 13:19:55 -0300

    correções

--- src/routes.js
+++ src/routes.js
@@ -26,8 +26,8 @@ module.exports = (app) => ({
         //Não acredito que seria muito util para implementação, mas temos que levar em conta
         //por que o trabalho de BD exigi as 12 consultas eu acho, não sei na verdade, se não
         //exigir podemos excluir
-        app.get('/cliente/:consulta', controllers.consultas.cliente.envia);
-        app.get('/gerente/:consulta', controllers.consultas.gerente.envia);
-        app.get('/tecnico/:consulta', controllers.consultas.tecnico.envia);
+        app.get('/cliente/:consulta', controllers.cliente.envia);
+        app.get('/gerente/:consulta', controllers.gerente.envia);
+        app.get('/tecnico/:consulta', controllers.tecnico.envia);
     }
 });
